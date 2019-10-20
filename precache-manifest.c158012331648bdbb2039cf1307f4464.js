db0f7542e62f4e4279fd16fee964b89bc6f76bdd
DexMP <matveymp@yandex.ru>
2019-10-20 14:28:40 +0300

    Updates

---
+++ precache-manifest.c158012331648bdbb2039cf1307f4464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52ffffb3917390a6bd722f3aa0b3b7f",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c34776bab71e24423ba9",
+    "url": "/weather/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "e30d0b0abfb7dd4801a3",
+    "url": "/weather/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "c34776bab71e24423ba9",
+    "url": "/weather/static/js/2.26f047d5.chunk.js"
+  },
+  {
+    "revision": "e30d0b0abfb7dd4801a3",
+    "url": "/weather/static/js/main.246f437f.chunk.js"
+  },
+  {
+    "revision": "2175faabad6d52737106",
+    "url": "/weather/static/js/runtime-main.b2c53c11.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/weather/static/media/persik.4e1ec840.png"
+  }
+]);
